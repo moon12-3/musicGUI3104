2adb3331a66989a10d8c44fc9dc141ae0527fc81
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-10 14:25:37 +0900

    버튼 이벤트가 제대로 작동하지 않는 오류 수정

--- musicGUI3104/Form1.Designer.cs
+++ musicGUI3104/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.genreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.playCntDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.singerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.musicBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.musicBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -60,52 +60,6 @@
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(776, 430);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(13, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 90);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "전체 보기";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(169, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 90);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "악뮤 노래 보기";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(325, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 90);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "팝송 보기";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(481, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(150, 90);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "재생 수 1000회 이상 보기";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(639, 12);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 90);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "재생 수 많은 순으로 정렬";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // nameDataGridViewTextBoxColumn
             // 
@@ -138,6 +92,56 @@
             // musicBindingSource
             // 
             this.musicBindingSource.DataSource = typeof(musicGUI3104.Music);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 90);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "전체 보기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 90);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "악뮤 노래 보기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "팝송 보기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(481, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 90);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "재생 수 1000회 이상 보기";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(639, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 90);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "재생 수 많은 순으로 정렬";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
