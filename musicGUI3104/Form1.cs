0b6f19ddb71af17a246ee79ac7dd78ccaff3fc07
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-10 14:12:06 +0900

    GUI 작업 완료

--- musicGUI3104/Form1.cs
+++ musicGUI3104/Form1.cs
@@ -16,5 +16,10 @@ namespace musicGUI3104
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
