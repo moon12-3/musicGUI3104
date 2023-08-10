3086a840650bb09521de831bf793d43c38c2929c
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-10 14:12:54 +0900

    Music class 생성

---
+++ musicGUI3104/Music.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicGUI3104
+{
+    internal class Music
+    {
+        public String Name { get; set; }
+        public String Genre { get; set; }
+        public int PlayCnt { get; set; }
+        public String Singer { get; set; }
+    }
+}
